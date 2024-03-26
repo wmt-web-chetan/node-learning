f1c58c0c190ff5ed2458e22341edcd55df437f3e
wmt-web-chetan <chetanb@webmobtech.com>
2024-03-26 12:25:30 +0530

    ecommerce-modal

---
+++ models/ecommerce/category.model.js
@@ -0,0 +1,11 @@
+import mongoose,{Schema} from "mongoose";
+
+const categorySchema=new Schema({
+     name:{
+        type: String,
+        required: true
+     }
+
+},{timestamps:true})
+
+export const Category=mongoose.model("Category",categorySchema)
