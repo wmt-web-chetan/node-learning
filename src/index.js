65132b6531a15632603f4d2a35af79513a37a31f
wmt-web-chetan <chetanb@webmobtech.com>
2024-03-27 15:28:25 +0530

    connect to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
-
+import connectDB from "./db/index.js";
 const app = express();
 
 dotenv.config();
+connectDB()
 
-app.get("/", (req, resp) => {
-  resp.send("hello world");
-});
 
-app.get("/user", (req, resp) => {
-  resp.send("<h1>name is chinmoy</h1>");
-});
 
-app.get("/login", (req, resp) => {
-  resp.send("you are login!!!");
-});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
