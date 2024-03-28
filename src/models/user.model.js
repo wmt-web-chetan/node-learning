4884306fc6f747e6d400d247287b6af75034aa2d
wmt-web-chetan <chetanb@webmobtech.com>
2024-03-28 10:51:20 +0530

    user model and video modal

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,15 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
   username: {
     type: String,
     required: true,
     trim: true,
+    index: true,
+    lowercase: true,
+    unique: true,
   },
   fullName: {
     type: String,
@@ -13,11 +18,18 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
+  watchHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+  ],
 
   refreshToken: {
     type: String,
@@ -32,4 +44,40 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRE,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRE,
+    }
+  );
+};
 export const User = mongoose.model("User", userSchema);
